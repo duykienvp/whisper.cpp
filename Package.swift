bea07cca4aec8334fffc37a0dc7c3a2d1ecbaeb8
Kien Nguyen <duykienvp@gmail.com>
2024-08-03 12:57:16 -0700

    Update package for iOS 17

--- Package.swift
+++ Package.swift
@@ -1,14 +1,12 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "whisper",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v14),
-        .watchOS(.v4),
-        .tvOS(.v14)
+        .macOS(.v14),
+        .iOS(.v17),
     ],
     products: [
         .library(name: "whisper", targets: ["whisper"]),
@@ -35,20 +33,18 @@ let package = Package(
                 "ggml/src/ggml-quants.c",
                 "ggml/src/ggml-metal.m"
             ],
-            resources: [.process("ggml/src/ggml-metal.metal")],
+            resources: [.process("ggml-metal.metal")],
             publicHeadersPath: "spm-headers",
             cSettings: [
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
                 .define("GGML_USE_METAL"),
-                .define("WHISPER_USE_COREML"),
-                .define("WHISPER_COREML_ALLOW_FALLBACK")
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
                 // We should consider add this in the future when we drop support for iOS 14
                 // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
-                // .define("ACCELERATE_NEW_LAPACK"),
-                // .define("ACCELERATE_LAPACK_ILP64")
+                 .define("ACCELERATE_NEW_LAPACK"),
+                 .define("ACCELERATE_LAPACK_ILP64")
             ],
             linkerSettings: [
                 .linkedFramework("Accelerate")
